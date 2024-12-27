22bcb70e5514cb33579d3ef85c7c45fdc760b239
cameronotoole44 <cameronotoole404@gmail.com>
2024-12-26 21:33:45 -0500

    using router/routes

--- red-dead-tribute/src/App.tsx
+++ red-dead-tribute/src/App.tsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/General/Home";
 import Navbar from "./Components/General/Navbar";
+import Locations from "./Components/Locations/Locations";
+import Footer from "./Components/General/Footer";
 
 const App: React.FC = () => {
   return (
-    <div className="app-container">
-      <Navbar />
-      <Home />
-    </div>
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/locations" element={<Locations />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
