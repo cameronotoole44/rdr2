77c63397eaf5d4012522df674210b8fba979bfa0
cameronotoole44 <cameronotoole404@gmail.com>
2024-12-27 16:03:51 -0500

    added overview and gallery section

--- red-dead-tribute/src/App.tsx
+++ red-dead-tribute/src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/General/Home";
 import Navbar from "./Components/General/Navbar";
 import Locations from "./Components/Locations/Locations";
+import Overview from "./Components/About/Overview";
+import Gallery from "./Components/About/Gallery";
 import Footer from "./Components/General/Footer";
 
 const App: React.FC = () => {
@@ -13,6 +15,8 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/locations" element={<Locations />} />
+          <Route path="/overview" element={<Overview />} />
+          <Route path="/gallery" element={<Gallery />} />
         </Routes>
         <Footer />
       </div>
