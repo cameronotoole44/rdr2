382c23b6d24da9074825d037181678d6a7c41c90
cameronotoole44 <cameronotoole404@gmail.com>
2024-12-31 20:38:55 -0500

    wildlife/legend/fish and gang added

--- red-dead-tribute/src/App.tsx
+++ red-dead-tribute/src/App.tsx
@@ -5,6 +5,10 @@ import Navbar from "./Components/General/Navbar";
 import Locations from "./Components/Locations/Locations";
 import Overview from "./Components/About/Overview";
 import Gallery from "./Components/About/Gallery";
+import WildlifeGrid from "./Components/Wildlife/Wildlife";
+import FishGrid from "./Components/Wildlife/Fish";
+import LegendaryGrid from "./Components/Wildlife/Legendary";
+import Vanderlinde from "./Components/Gang/VanDerLinde";
 import Footer from "./Components/General/Footer";
 
 const App: React.FC = () => {
@@ -17,6 +21,10 @@ const App: React.FC = () => {
           <Route path="/locations" element={<Locations />} />
           <Route path="/overview" element={<Overview />} />
           <Route path="/gallery" element={<Gallery />} />
+          <Route path="/wildlife" element={<WildlifeGrid />} />
+          <Route path="/fish" element={<FishGrid />} />
+          <Route path="/legendary" element={<LegendaryGrid />} />
+          <Route path="/the-gang" element={<Vanderlinde />} />
         </Routes>
         <Footer />
       </div>
